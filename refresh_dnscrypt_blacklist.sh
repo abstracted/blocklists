e0c0776345502eac88ca3380d5f8749109a18da4
Cameron <csanders@protonmail.com>
2020-07-28 21:37:47 -0500

    change blacklist

--- refresh_dnscrypt_blacklist.sh
+++ refresh_dnscrypt_blacklist.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 blacklist_file="/etc/dnscrypt-proxy/blacklist.txt"
-blacklist_generated="${dir}/_domains_blacklist_generated.txt"
+whitelist_file="/etc/dnscrypt-proxy/whitelist.txt"
+
+blacklist="${dir}/domains-blacklist.txt"
+whitelist="${dir}/domains-allowlist.txt"
 
 # Generate the blacklist
-./generate-domains-blocklist.py -o $blacklist_generated
+./generate-domains-blocklist.py -o $blacklist
 
 # Copy the blacklist
-sudo cat $blacklist_generated | sudo tee $blacklist_file
-
-# TODO: need to omit files removed from local additions
-# sudo cat $blacklist_file $blacklist_generated \
-# 	| sort \
-# 	| uniq -u \
-# 	| sudo tee $blacklist_file
+sudo cat $blacklist | sudo tee $blacklist_file
+# Copy the whitelist
+sudo cat $whitelist | sudo tee $whitelist_file
 
 # Refresh the service
 sudo systemctl restart dnscrypt-proxy.service
